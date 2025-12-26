7a88dcf6b2068f28de14e073a3aca7c075f7bed5
Insoninia <guragaidipendra6@gmail.com>
2025-12-26 14:09:12 +0545

    change in db file

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/ticketNepal`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/TicketNepal1`);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);

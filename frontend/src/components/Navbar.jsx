33366b569e97572cbee6ba89ae71e1ea0a13e549
Insoninia <guragaidipendra6@gmail.com>
2025-12-26 14:33:31 +0545

    change in the navbar.js file for backend purpose

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { assets } from "../assets/assets";
 import {
   MenuIcon,
   SearchIcon,
+  TicketCheck,
   TicketPlus,
   XIcon,
 } from "lucide-react";
@@ -13,27 +14,33 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { user } = useUser();
   const { openSignIn } = useClerk();
-  const navigate = useNavigate(); // ✅ FIXED
+  const navigate = useNavigate();
 
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-6 lg:px-36 py-5">
-      <Link to="/" className="max-md:flex-1"> {/* ✅ FIXED */}
+      <Link className="max-md:flex-1">
         <img src={assets.logo} alt="" className="w-36 h-auto" />
       </Link>
 
       <div
         className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium
-        z-50 flex flex-col md:flex-row items-center
-        max-md:justify-center gap-8 mid-md:px-8 py-3 max-md:h-screen
-        min-md:rounded-full backdroup-blur bg-black/70 md:bg-white/10 md:border
-        border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
-          isOpen ? "max-md:w-full" : "max-md:w-0"
-        }`}
+       max-md:test-lg z-50 flex flex-col md:flex-row items-center
+       max-md:justify-center gap-8 mid-md:px-8 py-3 max-md:h-screen
+       min-md:rounded-full backdroup-blur bg-black/70 md:bg-white/10 md:border
+       border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
+         isOpen ? "max-md:w-full" : "max-md:w-0"
+       }`}
       >
         <XIcon
           className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         />
+
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to="/">Home</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to="/movies">Movies</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to="/">Theaters</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to="/">Releases</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to="/favorite">Favouriets</Link>
       </div>
 
       <div className="flex item-center gap-8">
@@ -51,8 +58,8 @@ const Navbar = () => {
             <UserButton.MenuItems>
               <UserButton.Action
                 label="My Bookings"
-                labelIcon={<TicketPlus width={15} />} {/* ✅ FIXED */}
-                onClick={() => navigate("/my-bookings")} {/* ✅ WORKS NOW */}
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
               />
             </UserButton.MenuItems>
           </UserButton>
